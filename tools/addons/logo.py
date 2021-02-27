db4ccda23eb6fe69f148b858176ce78438c2f45d
Харун <77844554+XapyH@users.noreply.github.com>
2021-02-27 20:09:00 +0300

    Tools
    Tools

---
+++ tools/addons/logo.py
@@ -0,0 +1,14 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+ __   __                 _    _ 
+ \ \ / /                | |  | |
+  \ V / __ _ _ __  _   _| |__| |
+   > < / _` | '_ \| | | |  __  |
+  / . \ (_| | |_) | |_| | |  | |
+ /_/ \_\__,_| .__/ \__, |_|  |_|
+            | |     __/ |       
+            |_|    |___/        Coder by XapyH
+{Fore.RESET}""")
